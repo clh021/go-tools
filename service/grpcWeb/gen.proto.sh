1703b8651c2fa40ccb8a1e13cea9f341a9d194a3
chenlianghong <clh021@gmail.com>
2022-06-13 16:01:20 +0800

    gen.proto: update

--- service/grpcWeb/gen.proto.sh
+++ service/grpcWeb/gen.proto.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
-docker run \
-    -v `pwd`/api:/api \
-    -v `pwd`/time/goclient:/goclient \
-    -v `pwd`/frontend/src/jsclient:/jsclient \
+docker run --rm \
+    --user "$(id -u):$(id -g)" \
+    -v "$(pwd)"/todo:/api \
+    -v "$(pwd)"/todo:/goclient \
+    -v "$(pwd)"/web:/jsclient \
     leehom/grpc-web-generators \
     protoc -I /api \
         --go_out=plugins=grpc,paths=source_relative:/goclient \
         --js_out=import_style=commonjs:/jsclient \
         --grpc-web_out=import_style=commonjs,mode=grpcwebtext:/jsclient \
-        /api/time/v1/time_service.proto
+        /api/todo.proto
