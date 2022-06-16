05a02f1a7e747eac004cb7826a82c257f9fca78e
chenlianghong <clh021@gmail.com>
2022-06-16 15:21:40 +0800

    grpcVue: add server code from grpcWeb

---
+++ service/grpcVue/echoing/handler.go
@@ -0,0 +1,69 @@
+package echoing
+
+import (
+	"fmt"
+	"golang.org/x/net/context"
+	"log"
+)
+
+type Server struct {
+}
+
+func (s *Server) Echo(ctx context.Context, req *EchoRequest) (*EchoResponse, error) {
+	log.Printf("Received new echo request %s", req)
+	echoObject := &EchoResponse{
+		Message:      fmt.Sprintf("receive:Echo: %s", req),
+		MessageCount: 1,
+	}
+	return echoObject, nil
+}
+
+func (s *Server) EchoAbort(ctx context.Context, req *EchoRequest) (*EchoResponse, error) {
+	log.Printf("Received new EchoAbort request %s", req)
+	return &EchoResponse{
+		Message:      fmt.Sprintf("receive:EchoAbort: %s", req),
+		MessageCount: 2,
+	}, nil
+}
+func (s *Server) NoOp(ctx context.Context, req *Empty) (*Empty, error) {
+	log.Printf("Received new NoOp request %s", req)
+	return &Empty{}, nil
+}
+func (s *Server) ServerStreamingEcho(ctx *ServerStreamingEchoRequest, stream EchoService_ServerStreamingEchoServer) error {
+	log.Printf("Received new ServerStreamingEcho request")
+	return nil
+}
+func (s *Server) ServerStreamingEchoAbort(ctx *ServerStreamingEchoRequest, streamAbort EchoService_ServerStreamingEchoAbortServer) error {
+	log.Printf("Received new ServerStreamingEchoAbort request")
+	return nil
+}
+func (s *Server) ClientStreamingEcho(stream EchoService_ClientStreamingEchoServer) error {
+	log.Printf("Received new ClientStreamingEcho request")
+	return nil
+}
+
+func (s *Server) FullDuplexEcho(fullDuplex EchoService_FullDuplexEchoServer) error {
+	log.Printf("Received new FullDuplexEcho request")
+	return nil
+}
+func (s *Server) HalfDuplexEcho(halfDuplex EchoService_HalfDuplexEchoServer) error {
+	log.Printf("Received new HalfDuplexEcho request")
+	return nil
+}
+
+// func (s *Server) GetTodos(ctx context.Context, _ *GetTodoParams) (*TodoResponse, error) {
+// 	log.Printf("get tasks")
+// 	return &TodoResponse{Todos: s.Todos}, nil
+// }
+
+// func (s *Server) DeleteTodo(ctx context.Context, delTodo *DeleteTodoParams) (*DeleteResponse, error) {
+// 	var updatedTodos []*TodoObject
+// 	for index, todo := range s.Todos {
+// 		if todo.Id == delTodo.Id {
+// 			updatedTodos = append(s.Todos[:index], s.Todos[index+1:]...)
+// 			break
+// 		}
+// 	}
+// 	s.Todos = updatedTodos
+// 	return &DeleteResponse{Message: "success"}, nil
+// }
---
+++ service/grpcVue/server.go
@@ -0,0 +1,39 @@
+package grpcWeb
+
+import (
+	"fmt"
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+	"runtime/debug"
+	"test/service/grpcWeb/echoing"
+)
+
+func Main() {
+
+	// 退出自动恢复服务
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Printf("%s\n\n%s\n", r, debug.Stack())
+		}
+	}()
+
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", 18081))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	} else {
+		log.Printf("Server listen on port 0.0.0.0:18081.")
+	}
+
+	s := echoing.Server{}
+	grpcServer := grpc.NewServer()
+
+	echoing.RegisterEchoServiceServer(grpcServer, &s)
+
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %s", err)
+	} else {
+		log.Printf("Server started successfully")
+	}
+}
