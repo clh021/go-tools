7e99a1812a6a3e240c9fbb24b2b77a468d269909
chenlianghong <clh021@gmail.com>
2022-06-16 14:35:00 +0800

    grpcWeb: vue create proj

--- service/grpcWeb/vue/src/App.vue
+++ service/grpcWeb/vue/src/App.vue
@@ -1,119 +1,26 @@
 <template>
-  <div id="app">
-    <!-- <section>
-      <span class="title-text">TODO gRPC Client</span>
-      <div class="row justify-content-center mt-4">
-        <input v-model="inputField" v-on:keyup.enter="addTodo" class="mr-1" placeholder="Todo Item">
-        <button @click="addTodo" class="btn btn-primary">Add Todo</button>
-      </div>
-    </section>
-    <section>
-      <div class="row">
-        <div class="offset-md-3 col-md-6 mt-3">
-          <ul class="list-group justify-content-center">
-            <li
-              class="row list-group-item border mt-2 col-xs-1"
-              v-for="todo in todos"
-              v-bind:key="todo.id"
-            >
-              <div>
-                <span>{{todo.task}}</span>
-                <span @click="deleteTodo(todo)" class="offset-sm-1 col-sm-2 delete text-right">X</span>
-              </div>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </section> -->
-  </div>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
-import "bootstrap/dist/css/bootstrap.css";
-import "bootstrap-vue/dist/bootstrap-vue.css";
+import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: "app",
-  components: {},
-  data: function() {
-    return {
-      inputField: "",
-      todos: []
-    };
-  },
-  created: function() {
-    const {GetTodoRequest} = require('./lib/todos_pb.js');
-    const {TodoServiceClient} = require('./lib/todos_grpc_web_pb.js');
-
-    const todoService = new proto.todos.TodoServiceClient('http://localhost:8080');
-    const todoId = 1234;
-
-    var getTodoRequest = new proto.todos.GetTodoRequest();
-    getTodoRequest.setId(todoId);
-
-    var metadata = {};
-    var getTodo = todoService.getTodoById(getTodoRequest, metadata, (err, response) => {
-      if (err) {
-        console.log(err);
-      } else {
-        const todo = response.todo();
-        if (todo == null) {
-          console.log(`A TODO with the ID ${todoId} wasn't found`);
-        } else {
-          console.log(`Fetched TODO with ID ${todoId}: ${todo.content()}`);
-        }
-      }
-    });
-    // let serverAddr = this.getServerAddr();
-    // this.client = new todoServiceClient(serverAddr, null, null);
-    // this.getTodos();
-  },
-  methods: {
-    // getServerAddr: function() {
-    //   let serverAddr = `${document.location.origin}`;
-    //   console.log(serverAddr);
-    //   return serverAddr;
-    // },
-    // getTodos: function() {
-    //   let getRequest = new getTodoParams();
-    //   this.client.getTodos(getRequest, {}, (err, response) => {
-    //     this.todos = response.toObject().todosList;
-    //     console.log(this.todos);
-    //   });
-    // },
-    // addTodo: function() {
-    //   let request = new addTodoParams();
-    //   request.setTask(this.inputField);
-    //   this.client.addTodo(request, {}, () => {
-    //     this.inputField = "";
-    //     this.getTodos();
-    //   });
-    // },
-    // deleteTodo: function(todo) {
-    //   let deleteRequest = new deleteTodoParams();
-    //   deleteRequest.setId(todo.id);
-    //   this.client.deleteTodo(deleteRequest, {}, (err, response) => {
-    //     if (response.getMessage() === "success") {
-    //       this.getTodos();
-    //     }
-    //   });
-    //   console.log("todo -> ", todo.id);
-    // }
+  name: 'App',
+  components: {
+    HelloWorld
   }
-};
+}
 </script>
 
 <style>
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
-
-.title-text {
-  font-size: 22px;
 }
 </style>
