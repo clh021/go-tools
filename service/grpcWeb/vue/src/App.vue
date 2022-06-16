2c50d65cb4ddb3728bedf6f5b3e1eede8bc5925d
chenlianghong <clh021@gmail.com>
2022-06-16 14:57:58 +0800

    grpcWeb: support lib

--- service/grpcWeb/vue/src/App.vue
+++ service/grpcWeb/vue/src/App.vue
@@ -1,15 +1,37 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
 
+import { grpc } from "@improbable-eng/grpc-web";
+import { EchoService, EchoServiceClient } from "./lib/echo_pb_service";
+import { EchoRequest } from "./lib/echo_pb";
+const host = "http://192.168.1.68:18080";
+
 export default {
   name: 'App',
   components: {
     HelloWorld
+  },
+  created: () => {
+    const req = new EchoRequest();
+    const grpcRequest = grpc.unary(EchoService.Echo, {
+      request: req,
+      host: host,
+      onMessage: res => {
+        console.log("all ok. got onMessage: ", JSON.stringify(res));
+      },
+      onEnd: res => {
+        // const { status, statusMessage, headers, message, trailers } = res;
+        // if (status === grpc.Code.OK && message) {
+        //   console.log("all ok. got onEnd: ", message.toObject());
+        // }
+        console.log("all ok. got onEnd: ", JSON.stringify(res));
+      }
+    });
+    grpcRequest.close();
   }
 }
 </script>
