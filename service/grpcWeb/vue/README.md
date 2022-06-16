7fe361fca911b683199479cca9cc612a8727b12b
chenlianghong <clh021@gmail.com>
2022-06-16 14:47:40 +0800

    grpcWeb: add docker-compose build

--- service/grpcWeb/vue/README.md
+++ service/grpcWeb/vue/README.md
@@ -22,3 +22,9 @@ npm run lint
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+
+
+
+当发现生成的代码 有错误 "Uncaught ReferenceError: exports is not defined" 时
+删除vue项目根目录的 babel.config.js 文件可消除此问题
---
+++ service/grpcWeb/vue/gen.proto.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+webPath=$(pwd)
+servicePath="$(dirname "$webPath")"
+docker run --rm \
+    --user "$(id -u):$(id -g)" \
+    -v "${servicePath}"/echoing:/api \
+    -v "${servicePath}"/echoing:/goclient \
+    -v "${servicePath}"/vue/src/lib:/jsclient \
+    leehom/grpc-web-generators \
+    protoc -I /api \
+        --plugin=protoc-gen-ts=/usr/local/bin/protoc-gen-ts \
+        --js_out=import_style=commonjs,binary:/jsclient \
+        --ts_out=service=grpc-web:/jsclient \
+        /api/echo.proto
+
+# protoc \
+#   --plugin=protoc-gen-ts=/usr/local/bin/protoc-gen-ts \
+#   --plugin=protoc-gen-go=/golang/go/bin/protoc-gen-go \
+#   -I ./api \
+#   --js_out=import_style=commonjs,binary:/jsclient \
+#   --go_out=plugins=grpc:/goclient \
+#   --ts_out=service=grpc-web:/jsclient \
+#   /api/echo.proto
+
+# protoc -I /api \
+#     --go_out=plugins=grpc,paths=source_relative:/goclient \
+#     --js_out=import_style=commonjs:/jsclient \
+#     --grpc-web_out=import_style=commonjs,mode=grpcwebtext:/jsclient \
+#     /api/echo.proto
