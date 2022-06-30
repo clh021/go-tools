046dc2f4e650bbd7b3ae0db342a72f4ad696b69e
chenlianghong <clh021@gmail.com>
2022-06-30 21:01:01 +0800

    grpcWS: chat proto&gens

--- service/grpcWS/gen.proto.sh
+++ service/grpcWS/gen.proto.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 docker run --rm \
     --user "$(id -u):$(id -g)" \
-    -v "$(pwd)"/echoing:/api \
-    -v "$(pwd)"/echoing:/goclient \
-    -v "$(pwd)"/web/lib:/jsclient \
+    -v "$(pwd)"/chat:/api \
+    -v "$(pwd)"/chat:/goclient \
+    -v "$(pwd)"/html/lib:/jsclient \
     leehom/grpc-web-generators \
     protoc -I /api \
         --go_out=plugins=grpc,paths=source_relative:/goclient \
         --js_out=import_style=commonjs:/jsclient \
         --grpc-web_out=import_style=commonjs,mode=grpcwebtext:/jsclient \
-        /api/echo.proto
+        /api/chat.proto
