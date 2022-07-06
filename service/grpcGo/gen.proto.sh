b45df979198636c61ba154222d83b588619f6faa
chenlianghong <clh021@gmail.com>
2022-07-06 16:44:34 +0800

    grpcGo: gen.proto.sh update comment

--- service/grpcGo/gen.proto.sh
+++ service/grpcGo/gen.proto.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+# leehom Chen clh021@gmail.com
+# 1. https://github.com/protocolbuffers/protobuf/releases
+# 2. go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
 protoc --go_out=. --go_opt=paths=source_relative \
     --go-grpc_out=. --go-grpc_opt=paths=source_relative \
     protos/helloworld.proto
